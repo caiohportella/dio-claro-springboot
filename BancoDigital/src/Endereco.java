f5b2f1f00d729607a253f576577452e32d4e28d6
Caio Portella <caiohportella@gmail.com>
2024-08-06 18:36:23 -0300

    implemented digital bank

---
+++ BancoDigital/src/Endereco.java
@@ -0,0 +1,9 @@
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Endereco {
+    private String endereco;
+    private String bairro;
+    private String cidade;
+    private String uf;
+}
---
+++ BancoDigital/src/TipoDeConta.java
@@ -0,0 +1,13 @@
+public enum TipoDeConta {
+    CONTA_CORRENTE("Conta Corrente"),
+    CONTA_POUPANÇA("Conta Poupança");
+
+    private String  tipoDeConta;
+    TipoDeConta(String tipoDeConta) {
+        this.tipoDeConta = tipoDeConta;
+    }
+
+    public String getTipoDeConta(){
+        return this.tipoDeConta;
+    }
+}
